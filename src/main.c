f7eef96f615ff116530dbed2fd9ccb928de9ccf4
morgashen <mashudumorgan@gmail.com>
2024-05-02 19:34:06 +0000

    maze-project

---
+++ src/main.c
@@ -0,0 +1,51 @@
+#include "../include/main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+    SDL_Instance instance;
+    SDL_Event event = {0};
+    Uint8 keys[SDL_NUM_SCANCODES] = {0};
+    Vector player;
+    double time = 0;
+    double oldTime = 0;
+    bool quit = false;
+
+    player.dirX = -1;
+    player.dirY = 0;
+    player.posX = 22;
+    player.posY = 12;
+    player.planeX = 0;
+    player.planeY = 0.66;
+
+    /* Start up SDL and create window */
+    if (!initialize_SDL(&instance))
+    {
+        fprintf(stderr, "Failed to initialize!\n");
+    }
+    else
+    {
+        while (!quit)
+        {
+            SDL_SetRenderDrawColor(instance.renderer, 0x0, 0x0, 0x0, 0x0);
+            SDL_RenderClear(instance.renderer);
+
+            readKeys(keys);
+            quit = done(&event, true, keys);
+
+            if (quit)
+                break;
+
+            raycaster(player, &time, &oldTime, &instance, &event, true, keys);
+            SDL_RenderPresent(instance.renderer);
+        }
+
+        end(&instance);
+    }
+
+    return 0;
+}
---
+++ src/window_status.c
@@ -0,0 +1,43 @@
+#include "../include/main.h"
+
+/**
+ * done - A function that returns 1 if you close the window or press the escape
+ * key. Also handles everything that's needed per frame.
+ *
+ * @event: An SDL Event
+ * @delay: The delay flag (true or false)
+ * @keys: A pointer to a boolean array to store key states
+ * Return: Boolean success flag (true or false)
+ */
+bool done(SDL_Event *event, bool delay, const Uint8 *keys)
+{
+    /* delay gives CPU some free time */
+    /* use once per frame to avoid 100% usage of a CPU core */
+    if (delay)
+        SDL_Delay(5); /* so it consumes less processing power */
+
+    SDL_PollEvent(event);
+
+    /* Check for quit event or Escape key press */
+    if (event->type == SDL_QUIT || keys[SDL_SCANCODE_ESCAPE])
+        return true;
+
+    return false;
+}
+
+/**
+ * readKeys - A function that gives value of pressed key to keys array.
+ *
+ * @keys: A pointer to a boolean array to store key states
+ */
+void readKeys(Uint8 *keys)
+{
+    /* Update the event queue */
+    SDL_PumpEvents();
+
+    /* Get the current state of the keyboard */
+    const Uint8 *keystate = SDL_GetKeyboardState(NULL);
+
+    /* Copy the key states to the provided array */
+    memcpy(keys, keystate, SDL_NUM_SCANCODES);
+}
